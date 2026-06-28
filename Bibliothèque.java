d00cf9c92773564281d6a16ae73e0bcb61ea7465
LY Bocar Thierno <thiernoly385@gmail.com>
2026-06-28 17:09:21 +0000

    Ajout de la classe Bibliothèque

---
+++ Bibliothèque.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Bibliotheque {
+    private ArrayList<Livre> livres;
+
+    public Bibliotheque(){
+        this.livres = new ArrayList<Livre>();
+    }
+
+    public ArrayList<Livre> getLivres() {
+        return livres;
+    }
+
+    public void ajouterLivre(Livre livre) {
+        livres.add(livre);
+    }
+
+    public boolean supprimerLivre(Livre livre) {
+        return livres.remove(livre);
+    }
+
+    public void afficherLivres() {
+        for(Livre livre : livres){
+            System.out.println(livre);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bibliotheque{livres=" + livres + "}";
+    }
+}
