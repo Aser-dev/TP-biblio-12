a748ff94362a9cfa963003f3c92b3c333862ddc0
Achimelec Coulibaly <melecdevinnov@gmail.com>
2026-06-28 16:42:01 +0000

    Ajout de la classe Livre.java avec titre auteur et isbn

---
+++ Livre.java
@@ -0,0 +1,25 @@
+public class Livre{
+    private String titre;
+    private String auteur;
+    private String isbn;
+
+    public Livre(String titre, String auteur, String isbn){
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+    }
+    public String getTitre() {
+        return titre;
+    }
+    public String getAuteur() {
+        return auteur;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    @Override
+    public String toString() {
+        return "Livre{titre='" + titre + "', auteur='" + auteur + "', isbn ='" + isbn + "'}";
+    }
+
+}
