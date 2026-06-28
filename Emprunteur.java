eaa9e079b68a7221c6e8f49748cca209d3c30d58
Achimelec Coulibaly <melecdevinnov@gmail.com>
2026-06-28 16:43:36 +0000

    Ajout de la classe Emprunteur.java avec nom numero et numeroAdherant

---
+++ Emprunteur.java
@@ -0,0 +1,27 @@
+public class Emprunteur{
+    private String nom;
+    private String prenom;
+    private String numeroAdherant;
+
+    public Emprunteur(String nom, String prenom, String numeroAdherant){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroAdherant = numeroAdherant;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public String getNumeroAdherant() {
+        return numeroAdherant;
+    }
+    @Override
+    public String toString() {
+        return "Emprunteur{Nom='" + nom + "', Prenom='" + prenom + "', NumeroAdherant ='" + numeroAdherant + "'}";
+    }
+
+}     
+    
+
